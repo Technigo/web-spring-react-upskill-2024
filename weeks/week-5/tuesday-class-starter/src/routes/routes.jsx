980a0bdec570c7c839e18b76c76be8ba0b057e9d
Diego Zito <diegozito.m@gmail.com>
2024-03-19 14:09:15 +0100

    update - w5 finnished  added

--- weeks/week-5/tuesday-class-starter/src/routes/routes.jsx
+++ weeks/week-5/tuesday-class-starter/src/routes/routes.jsx
@@ -3,12 +3,14 @@ import { HomePage } from "../pages/HomePage";
 import { AboutPage } from "../pages/AboutPage";
 import { SongInfoPage } from "../pages/SongInfoPage";
 import { MyInfoPage } from "../pages/MyInfoPage";
+import { NotFoundPage } from "../pages/NotFoundPage";
 
 export const routes = (
   <>
     <Route path="/" element={<HomePage />} />
     <Route path="/about" element={<AboutPage />} />
     <Route path="/my-info" element={<MyInfoPage />} />
+    <Route path="*" element={<NotFoundPage />} />
     {/* DYNAMIC ROUTING */}
     <Route path="/song/:songTitle" element={<SongInfoPage />} />
   </>
